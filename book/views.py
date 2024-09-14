ea7c6a1a80a8aae9e4ee6142b15c0d25989ccafe
ernistUrai <ernistturgunbaev01@gmail.com>
2024-09-14 23:56:21 +0500

    frist commit

---
+++ book/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+def hello_world_view(request):
+    return HttpResponse(
+        "<body bgcolor=red> <h1> My name is Bektur <h2/></body>"
+    )
+
+
+# post_view
+def post_view(request):
+    post = models.Post.objects.all()
+    return render(request, 'index.html',{'post':post})
